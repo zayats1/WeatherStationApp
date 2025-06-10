c20a12fb0a8084457e6770ba3f0e5ea3b2467d9e
zayats1 <zaiats.bogdan@gmail.com>
2025-06-10 21:05:28 +0300

    refactor code fo the bars

--- app/src/main/java/com/bogdandev/weatherstationapp/ui/MainScreen.kt
+++ app/src/main/java/com/bogdandev/weatherstationapp/ui/MainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -23,7 +24,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bogdandev.weatherstationapp.R
-import com.bogdandev.weatherstationapp.ui.theme.TheAppTheme
 
 
 @Composable
@@ -35,7 +35,7 @@ fun Weather(modifier: Modifier = Modifier) {
         {
             DateAndTimeBar(modifier = modifier)
             WeatherBar(
-                content = "Sunny\n temp: 20*C\n humidity: 80%\n Pressure: 101.325,",
+                data = "Sunny\n temp: 20*C\n humidity: 80%\n Pressure: 101.325,",
                 modifier = modifier
             )
             TemperatureBar(modifier = Modifier)
@@ -43,17 +43,38 @@ fun Weather(modifier: Modifier = Modifier) {
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun TemperatureBar(modifier: Modifier = Modifier){
+fun WeatherPreview() {
+    Surface (modifier = Modifier.padding(top = 25.dp).height(2000.dp)
+        .width(1080.dp)){
+        Weather(Modifier)
+    }
+}
+
+
+@Composable
+fun DisplayBar(content: @Composable RowScope.(modifier:Modifier) -> Unit, modifier: Modifier = Modifier){
     Row (
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
         modifier = modifier
-        .padding(5.dp)
-        .padding(top = 5.dp)
-        .fillMaxWidth()
-        .height(150.dp)) {
+            .padding(5.dp)
+            .padding(top = 5.dp)
+            .fillMaxWidth()
+            .height(150.dp)) {
+            content(modifier)
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun TemperatureBar(modifier: Modifier = Modifier){
+    DisplayBar(modifier = modifier,
+        content = {
         Text(
             text = "20*C",
             modifier = modifier.padding(start = 10.dp, end = 20.dp)
@@ -70,46 +91,30 @@ fun TemperatureBar(modifier: Modifier = Modifier){
 
             contentDescription = null
         )
-    }
+    })
 }
+
 
 
 
 @Preview(showBackground = true)
 @Composable
 fun DateAndTimeBar(modifier: Modifier = Modifier){
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center,
-        modifier = modifier
-            .padding(5.dp)
-            .padding(top = 5.dp)
-            .fillMaxWidth()
-            .height(50.dp)
-    ) {
-        Text(
-            text = "Lviv 16:36  07.07.2025",
-            modifier = modifier.padding(start = 10.dp, end = 20.dp)
-        )
-    }
+     DisplayBar(modifier = modifier.height(50.dp), content = {
+         Text(
+             text = "Lviv 16:36  07.07.2025",
+             modifier = modifier.padding(start = 10.dp, end = 20.dp)
+         )
+     })
 }
 
 
-
+@Preview(showBackground = true)
 @Composable
-fun WeatherBar(content: String, modifier: Modifier = Modifier) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center,
-        modifier = modifier
-            .padding(1.dp)
-            .padding(top = 5.dp)
-            .fillMaxWidth()
-            .height(250.dp)
-    ) {
-
+fun WeatherBar( modifier: Modifier = Modifier,data: String = "Lviv 20*C Sunny") {
+    DisplayBar(modifier = modifier, content =  {
         Text(
-            text = content,
+            text = data,
             modifier = modifier.padding(end = 20.dp)
         )
 
@@ -126,25 +131,10 @@ fun WeatherBar(content: String, modifier: Modifier = Modifier) {
 
             contentDescription = null
         )
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun WeatherPreview() {
-    Surface (modifier = Modifier.padding(top = 25.dp).height(2000.dp)
-        .width(1080.dp)){
-        Weather(Modifier)
-    }
+    })
 }
 
 
 
-@Preview(showBackground = true)
-@Composable
-fun WeatherBarPreview() {
-    TheAppTheme {
-        WeatherBar("Lviv 20*C Sunny")
-    }
-}
+
+
