94255baf6d27ede1118c2237a1aac461bb413b0e
zayats1 <zaiats.bogdan@gmail.com>
2025-06-11 18:07:26 +0300

    skeleton

--- app/src/main/java/com/bogdandev/weatherstationapp/app/WeatherStationViewModel.kt
+++ app/src/main/java/com/bogdandev/weatherstationapp/app/WeatherStationViewModel.kt
@@ -1,15 +1,31 @@
 package com.bogdandev.weatherstationapp.app
 
-import androidx.compose.runtime.getValue
-
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import android.util.Half
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class WeatherStationViewModel: ViewModel() {
-      var weatherInfo by mutableStateOf(WeatherInfo())
+const val REQUEST_INTERVAL_MS:Long  =  30_000L
 
-    fun fetchWeatherInfo(){
 
+class WeatherStationViewModel : ViewModel() {
+    private var isActive = true
+    private var _weatherInfo = MutableStateFlow(WeatherInfo())
+    var weatherInfo = _weatherInfo.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+           fetchWeatherInfo()
+        }
+    }
+
+    private suspend fun fetchWeatherInfo() {
+        while (isActive) {
+            _weatherInfo.value = WeatherInfo()  //todo
+            delay(REQUEST_INTERVAL_MS)
+        }
     }
 }
