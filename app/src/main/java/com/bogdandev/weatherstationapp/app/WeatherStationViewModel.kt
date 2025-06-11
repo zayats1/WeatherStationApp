d9497c65a8d802291179f28f28e7452d8e69cdc5
zayats1 <zaiats.bogdan@gmail.com>
2025-06-11 21:31:49 +0300

    remove unnecessary get in url

--- app/src/main/java/com/bogdandev/weatherstationapp/app/WeatherStationViewModel.kt
+++ app/src/main/java/com/bogdandev/weatherstationapp/app/WeatherStationViewModel.kt
@@ -7,13 +7,14 @@ import io.ktor.client.HttpClient
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
+import io.ktor.client.request.request
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 const val REQUEST_INTERVAL_MS:Long  =  30_000L
-const val URL: String =  "http://192.168.1.1:80/get" // TODO url selector
+const val URL: String =  "http://192.168.1.1:80/" // TODO url selector
 
 class WeatherStationViewModel : ViewModel() {
     private var isActive = true
