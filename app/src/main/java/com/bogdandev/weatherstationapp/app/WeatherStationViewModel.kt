a60df949be0206778c61fcee18916043c2f66004
zayats1 <zaiats.bogdan@gmail.com>
2025-06-11 18:15:21 +0300

    todo

--- app/src/main/java/com/bogdandev/weatherstationapp/app/WeatherStationViewModel.kt
+++ app/src/main/java/com/bogdandev/weatherstationapp/app/WeatherStationViewModel.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 const val REQUEST_INTERVAL_MS:Long  =  30_000L
-
+const val URL: String =  "http://192.168.1.1:80/get"; // TODO url selector
 
 class WeatherStationViewModel : ViewModel() {
     private var isActive = true
